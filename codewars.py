d3c6e0a063cc121fc6a61075b074b3738ba10e85
00-hidan-00 <david.taranov@gmail.com>
2023-10-28 05:18:10 +0300

    Start project 🌟

---
+++ codewars.py
@@ -0,0 +1,11 @@
+class CodewarsLessons():
+
+    def __init__(self, ):
+        ...
+
+    def first_lesson(self=None):
+        ...
+
+
+if __name__ == '__main__':
+    CodewarsLessons.first_lesson()
---
+++ pyCheckiO.py
@@ -0,0 +1,11 @@
+class PyCheckiOLessons():
+
+    def __init__(self, ):
+        ...
+
+    def first_lesson(self=None):
+        ...
+
+
+if __name__ == '__main__':
+    PyCheckiOLessons.first_lesson()
