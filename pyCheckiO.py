21b8cc5107efa2e44a82534ae4c4bdcbd1252537
00-hidan-00 <david.taranov@gmail.com>
2023-10-29 19:16:18 +0200

    🧑‍🎓 Solutions to tasks on pyCheckiO: - Acceptable Password I - Number Length - Backward String

--- pyCheckiO.py
+++ pyCheckiO.py
@@ -9,7 +9,6 @@ class PyCheckiOLessons():
         This is an intro mission, the purpose of which is to explain how to solve missions on CheckiO.
         If you want to know how to get the maximum out of using CheckiO,
         check out our blog post "From Basic to Advance usage".
-
         This mission is the easiest one.
         Write a function that will receive 2 numbers as input and it should return the multiplication of these
         2 numbers.
@@ -24,9 +23,9 @@ class PyCheckiOLessons():
     def first_word(self, text: str) -> str:
         """
         You are given a string and you have to find its first word.
-
         The input string consists of only English letters and spaces.
         There aren’t any spaces at the beginning and the end of the string.
+        
         Input: A string (str).
 
         Output: A string (str).
@@ -46,8 +45,75 @@ class PyCheckiOLessons():
 
         return (num % 2) == 0
 
+    # Acceptable Password I
+    def is_acceptable_password(self, password: str) -> bool:
+        """
+        The verification condition is:
+        the length should be bigger than 6.
+        
+        Input: A string (str).
+
+        Output: A logic value (bool).
+        """
+        return len(password) > 6
+
+    # Number Length
+    def number_length(self, value: int) -> int:
+        """
+        You have a non-negative integer. Try to find out how many digits it has.
+
+        Input: A non-negative integer (int).
+
+        Output: An integer (int).
+        """
+        return len(str(value))
+
+    # Backward String
+    def backward_string(self, val: str) -> str:
+        """
+        You should return a given string in reverse order.
+
+        Input: A string (str).
+
+        Output: A string (str).
+        """
+
+        return val[::-1]
+
+    # The Most Frequent
+    def most_frequent(self, data: list[str]) -> str:
+        """
+        You have a sequence of strings, and you’d like to determine the most frequently occurring string
+        in the sequence. It can be the only one.
+
+        Input: Non-empty list of string (str).
+
+        Output: A string (str).
+        """
+
+        return max(data, key=data.count)
+
+    # Sum Numbers
+    def sum_numbers(self, text: str) -> int:
+        """
+        In a given text you need to sum the numbers while excluding any digits that form part of a word.
+        The text consists of numbers, spaces and letters from the English alphabet.
+
+        Input: A string (str).
+
+        Output: An integer (int).
+        """
+
+        # integer_text = 0
+        # 
+        # for string in text:
+        #     if string.isdigit():
+        #         integer_text += int(string)
+
+        return sum(int(string) for string in text.split(' ') if string.isdigit())
+
 
 if __name__ == '__main__':
     pyCheckiO_lessons = PyCheckiOLessons()
-    result = pyCheckiO_lessons.is_even(num=110)
+    result = pyCheckiO_lessons.sum_numbers("23 my 23 numbers 1st is ")
     print(result)
