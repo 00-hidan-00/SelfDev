788e1b47bb249f9347750ac8615f1193b5bd82b4
00-hidan-00 <david.taranov@gmail.com>
2023-10-29 19:21:03 +0200

    🛠 Edit codewars.py

--- codewars.py
+++ codewars.py
@@ -3,12 +3,14 @@ class CodewarsLessons():
     def __init__(self, ):
         ...
 
-    def first_lesson(self=None):
+    def first_lesson(self):
         ...
 
-    def second_lesson(self=None):
+    def second_lesson(self):
         ...
 
 
 if __name__ == '__main__':
-    CodewarsLessons.first_lesson()
+    codewars_lessons = CodewarsLessons()
+    result = codewars_lessons.first_lesson()
+    print(result)
